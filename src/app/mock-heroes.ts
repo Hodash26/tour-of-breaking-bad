21973cf8b058f5ede5432688530268cb3ed18cb7
Hodash26 <florent.bailly62400@gmail.com>
2019-11-06 15:11:35 +0100

    add css, list and hero editor

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Walter White'},
+    { id: 2, name: 'Jessy Pinkman'},
+    { id: 3, name: 'Saul Goodman'},
+    { id: 4, name: 'Gustavo Fring'},
+    { id: 5, name: 'Hank Schrader'},
+];
